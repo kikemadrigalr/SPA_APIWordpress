a0d540eb8498a805e89d4a61ea004ec4eddfa596
kikemadrigal <carlosmad03@gmail.com>
2023-04-09 22:28:18 -0500

    creacion de los primeros componentes de UI

--- app/App.js
+++ app/App.js
@@ -2,24 +2,39 @@
 
 import api from "./helpers/wp_api.js" //importar los endpoints de a API
 import { ajax } from "./helpers/ajax.js" // importar funcion para peticiones ajax
+import { Title } from "./components/Title.js";
+import { Loader } from "./components/Loader.js";
 
 export function App(){
-    document.getElementById("root").innerHTML = "<h1>Bienvenidos a mi primer SPA con Vanila JS</h1>";
+ const d = document,
+    $root = d.getElementById("root");
 
-    console.log(api);
+    $root.appendChild(Title());
+    $root.appendChild(Loader());
 
-    ajax({
-        url : api.POSTS,
-        cbSuccess : (posts) => {
-            console.log(posts);
-        }
-    });
-
-    ajax({
-        url : api.CATEGORIES,
-        cbSuccess : (category) => {
-            console.log(category);
-        }
-    });
+    // ajax({
+    //     url: "no-valid",
+    //     cdSucces : () => {}
+    // });
 }
+
+// export function App(){
+//     document.getElementById("root").innerHTML = "<h1>Bienvenidos a mi primer SPA con Vanila JS</h1>";
+
+//     console.log(api);
+
+//     ajax({
+//         url : api.POSTS,
+//         cbSuccess : (posts) => {
+//             console.log(posts);
+//         }
+//     });
+
+//     ajax({
+//         url : api.CATEGORIES,
+//         cbSuccess : (category) => {
+//             console.log(category);
+//         }
+//     });
+// }
 
